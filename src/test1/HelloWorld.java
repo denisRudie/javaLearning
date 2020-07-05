4547704f308c0f794458c55a32c8dc0387e2c00d
denisRudie <moonstamp1964@gmail.com>
2020-07-06 00:31:48 +0300

    add 2 java files Hello world

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -1,7 +1,7 @@
 package test1;
 
 public class HelloWorld {
-    private static String s = "Hello, World1!";
+    private static String s = "Hello, World12!";
 
     public static void main(String[] args) {
         System.out.println(s);
