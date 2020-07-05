33b2d71de04e31a35625246ab86c7f0cabb1e343
denisRudie <43638954+denisRudie@users.noreply.github.com>
2020-07-06 00:29:12 +0300

    Update HelloWorld.java

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -1,7 +1,7 @@
 package test1;
 
 public class HelloWorld {
-    private static String s = "Hello, World!";
+    private static String s = "Hello, World";
 
     public static void main(String[] args) {
         System.out.println(s);
